ab5ed2606bd154dd88bc9885e744f0eb66ed90bd
nikymorg <nicole.morgan@gmail.com>
2018-08-09 17:26:52 -0400

    post-lecture

--- 19_oo_js/index.js
+++ 19_oo_js/index.js
@@ -1,24 +1,47 @@
 // by hand objects - no way!
-// const jane = {
-//   name: 'jane',
-//   role: 'TCF',
-//   hobby: 'skydiving',
-//   afterWork: afterWork
-// }
-//
-// const greg = {
-//   name: 'greg',
-//   role: 'TCF',
-//   hobby: 'playing trombone in a ska band',
-//   afterWork: afterWork
-// }
+[
+  {
+    name: 'jane',
+    role: 'TCF',
+    hobby: 'skydiving',
+    afterWork: afterWork
+  }, {
+    name: 'greg',
+    role: 'TCF',
+    hobby: 'playing trombone in a ska band',
+    afterWork: afterWork
+  }
+]
 
-function afterWork(friend) {
-  return `After work I enjoy ${this.hobby} with ${friend}.`
+// factory pattern - oldie but goodie
+function instructorFactory(name, role, hobby) {
+  const obj = {}
+  obj.name = name
+  obj.role = role
+  obj.hobby = hobby
+  return obj
 }
 
+// factory pattern with closure for class variables
+// outer function runs once to set the id variable
+// then returns the inner function which we save in instructorFactory
+// we use instructorFactory to make new instructors
+let instructorFactoryWithId = (function() {
+  let id = 0
 
-// es 5
+  return function(name, role, hobby) {
+    const obj = {}
+    obj.name = name
+    obj.role = role
+    obj.hobby = hobby
+    obj.id = ++id
+    return obj
+  }
+})()
+instructorFactoryWithId('jane', 'tcf', 'skydiving')
+
+// ES 5 - getting there
+// uses the 'new' keyword which gives our function access to the new instance as this
 function Instructor(name, role, hobby) {
   // instance variables
   this.name = name
@@ -26,14 +49,72 @@ function Instructor(name, role, hobby) {
   this.hobby = hobby
 }
 
-// instance method
+// we can add instance methods to all instances using the prototype
+function afterWork() {
+  return `After work I enjoy ${this.hobby}.`
+}
+
 Instructor.prototype.afterWork = afterWork
-const greg = new Instructor('greg', 'tcf', 'ska')
-const jane = new Instructor('jane', 'tcf', 'skydiving')
-// Instructor.all()
-// Instructor.size()
+
+let jane = new Instructor('jane', 'tcf', 'skydiving')
+jane.afterWork() // `After work I enjoy skydiving`
+
+// and with closures to get a class variable again
+function InstructorMaker() {
+  all = []
+
+  function getAll() {
+    return [...all]
+  }
+
+  function maker(name, role, hobby) {
+    // instance variables
+    this.name = name
+    this.role = role
+    this.hobby = hobby
+    all.push(this)
+  }
+
+  // class methods
+  return {all: getAll, maker: maker}
+}
 
 
+let InstructorClass = InstructorMaker()
+let Instructor = InstructorClass.maker
+new Instructor('jane', 'tcf', 'skydiving')
+InstructorClass.all()
 
+// ES 6 - now we are talking
 
+function instructorMaker() {
+  let all = []
+  return class {
+    constructor(name, role, hobby) {
+      // instance variables
+      this.name = name
+      this.role = role
+      this.hobby = hobby
+      all.push(this)
+    }
 
+    // instance method
+    afterWork() {
+      return `After work I enjoy ${this.hobby}.`
+    }
+
+    // class method
+    static afterHours() {
+      return 'all the instructors enjoy coding'
+    }
+
+    static getAll() {
+      return all.slice()
+    }
+  }
+}
+
+let Instructor = instructorMaker()
+
+new Instructor('greg', 'tcf', 'ska')
+new Instructor('jane', 'tcf', 'skydiving')
