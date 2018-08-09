7674e964f6a68e3506adb0c3af55383dc842520d
nikymorg <nicole.morgan@gmail.com>
2018-08-09 16:03:35 -0400

    mid lecture

---
+++ 19_oo_js/index.js
@@ -0,0 +1,42 @@
+// by hand objects - no way!
+// const jane = {
+//   name: 'jane',
+//   role: 'TCF',
+//   hobby: 'skydiving',
+//   afterWork: afterWork
+// }
+//
+// const greg = {
+//   name: 'greg',
+//   role: 'TCF',
+//   hobby: 'playing trombone in a ska band',
+//   afterWork: afterWork
+// }
+
+function afterWork(friend) {
+  return `After work I enjoy ${this.hobby} with ${friend}.`
+}
+
+
+// es 5
+function Instructor(name, role, hobby) {
+  // instance variables
+  let all = []
+  all.push(this)
+  this.name = name
+  this.role = role
+  this.hobby = hobby
+  this.all = all
+}
+
+// instance method
+Instructor.prototype.afterWork = afterWork
+const greg = new Instructor('greg', 'tcf', 'ska')
+const jane = new Instructor('jane', 'tcf', 'skydiving')
+// Instructor.all()
+// Instructor.size()
+
+
+
+
+
