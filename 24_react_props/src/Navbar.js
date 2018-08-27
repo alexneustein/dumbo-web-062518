caaa6f31c8be46c72027d536c377f9ff2d6eefb7
nikymorg <nicole.morgan@gmail.com>
2018-08-27 15:44:47 -0400

    react props

---
+++ 24_react_props/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default class Navbar extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Painterest App</h1>
+      </div>
+    )
+  }
+}
+
+// export default Navbar
